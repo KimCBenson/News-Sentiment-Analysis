62015844048b51d08409056f55cd99b7ef395732
Yin Bo Chen <yinbo.chen@mymail.champlain.edu>
2025-04-22 16:39:48 -0400

    updated code

--- Flask/app.py
+++ Flask/app.py
@@ -3,6 +3,10 @@ import os
 import sys
 from datetime import datetime
 import csv
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 
 
@@ -34,7 +38,28 @@ def articleanalysis():
 
 
     result = run_absa(word, article)
-    return render_template('forms.html', result=result)
+
+    # --- Safely extract values using regex ---
+    sentiment_dict = dict(re.findall(r'(\w+)\s*=\s*([\d.]+)', result))
+    sentiment_dict = {k: float(v) for k, v in sentiment_dict.items()}
+
+    # Plotting as before
+    labels = list(sentiment_dict.keys())
+    values = list(sentiment_dict.values())
+
+    plt.figure(figsize=(6, 4))
+    plt.bar(labels, values, color=['red', 'gray', 'green'])
+    plt.title('Sentiment Analysis')
+    plt.ylabel('Score')
+
+    # Save image
+    img_path = os.path.join("static", "sentiment_plot.png")
+    plt.savefig(img_path)
+    plt.close()
+
+    print(f"Saving image to: {img_path}")
+
+    return render_template('forms.html', result=result, image_url=img_path)
 
 @app.route('/cnndata/')
 def cnndata():
@@ -64,8 +89,23 @@ def foxdata():
     return render_template('cvsdisplay.html', data=fox_data)
 
 
-#@app.route('/articleanalysis/', methods=['GET', 'POST'], endpoint='articleanalysis')
+@app.route('/bbcdata/')
+def bbcdata():
+    with open("../dataset/raw_news_titles/bbc_news_titles.csv", 'r', encoding="utf-8") as bbc_file:
+        bbc_data = list(csv.reader(bbc_file))
+    return render_template('cvsdisplay.html', data=bbc_data)
 
+@app.route('/cbcdata/')
+def cbcdata():
+    with open("../dataset/raw_news_titles/cbc_news_titles.csv", 'r', encoding="utf-8") as cbc_file:
+        cbc_data = list(csv.reader(cbc_file))
+    return render_template('cvsdisplay.html', data=cbc_data)
+
+@app.route('/npydata/')
+def nypdata():
+    with open("../dataset/raw_news_titles/nyp_news_titles.csv", 'r', encoding="utf-8") as nyp_file:
+        nyp_data = list(csv.reader(nyp_file))
+    return render_template('cvsdisplay.html', data=nyp_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
